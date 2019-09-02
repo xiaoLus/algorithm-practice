1eeee5dddf27ef799a68c092e40a2ff795beb4a2
陆偲渊 <siyuan.lu@mixmarvel.com>
2019-09-02 15:38:50 +0800

    konva test

--- src/views/ponding/App.vue
+++ src/views/ponding/App.vue
@@ -1,47 +1,70 @@
 <template>
   <div id="app">
-    <v-stage :config="configKonva">
+    <v-stage :config="configStage">
       <v-layer>
-        <v-circle :config="configCircle"></v-circle>
+        <v-rect :config="item" v-for="(item,key) in configRect" :key="key"></v-rect>
       </v-layer>
     </v-stage>
+    <input v-model="inputStr" />
+    <button @click="start">开始</button>
   </div>
 </template>
 
 <script>
-
+const WIDTH = 50;
+const HEIGHT = 50;
 
 export default {
   name: 'app',
   components: {
     
   },
-  data() {
-    return {
-      configKonva: {
+  computed: {
+    configStage() {
+      return {
         width: 200,
         height: 200
-      },
-      configCircle: {
-        x: 100,
-        y: 100,
-        radius: 70,
-        fill: "red",
-        stroke: "black",
-        strokeWidth: 4
       }
+    }
+  },
+  data() {
+    return {
+      inputStr: '',
+      inputArr: [],
+      inputMax: null,
+      configRect: [{
+        x: 0,
+        y: 0,
+        width: WIDTH,
+        height: HEIGHT,
+        fill: "grey",
+        stroke: "black",
+        strokeWidth: 2
+      },
+      {
+        x: WIDTH,
+        y: HEIGHT,
+        width: WIDTH,
+        height: HEIGHT,
+        fill: "grey",
+        stroke: "black",
+        strokeWidth: 2
+      }]
+    }
+  },
+  methods: {
+    async start() {
+      await this.RectInit();
+
+    },
+    async RectInit() {
+      if(!this.inputStr) return;
+      this.inputArr = this.inputStr.split(',')
+      console.log(this.inputStr,this.inputArr.length)
     }
   }
 }
 </script>
+<style lang="scss" scoped>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
